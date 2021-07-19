3289648cd193658e85a365e59ab066f0d7a1ba91
ant_reyn <antreyn@udel.edu>
2021-07-19 14:22:03 -0400

    built basic react router app with one example of link to and the rest are button on clicks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {Switch, Route, useHistory, Link} from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
 
 function App() {
+  const history = useHistory();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <button>
+            <Link to="/about"> About Us </Link>
+          </button>
+          <button onClick={() => history.push("/contact")}>Contact Us</button>
+          <button onClick={() => history.push("/")}>Home Page</button>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </header>
     </div>
   );
